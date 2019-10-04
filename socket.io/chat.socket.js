fe151cd72a4f32ce15e62c41361ca23062f99401
phan.cao.khoa <phan.cao.khoa@B121909-PC>
2019-10-04 11:25:29 +0700

    update chat room and send interval posture message

--- socket.io/chat.socket.js
+++ socket.io/chat.socket.js
@@ -1,52 +1,51 @@
 var chatSocket = function (io) {
 
+    var arrayUsers = [];
+
     // Chatroom namespace
     var numUsers = 0;
     io.on('connection', (socket) => {
-        var addedUser = false;
-        //console.log(socket.id);  
+        var addedUser = false;       
         socket.RoomName = socket.id;
+        var listRooms = [];
 
-        // when the client emits 'new message', this listens and executes
-        socket.on('new message', (data) => {
-            // we tell the client to execute 'new message'
-            socket.broadcast.emit('new message', {
-                username: socket.username,
-                message: data
+        socket.on("client-send-Username", function (data) {
+            if (arrayUsers.indexOf(data) >= 0) {
+                socket.emit("server-send-register-fail");
+            } else {
+                arrayUsers.push(data);
+                socket.Username = data;
+                socket.emit("server-send-register-successed", data);
+                io.sockets.emit("server-send-listUser", arrayUsers);
+            }
+        });
+        socket.on("logout", function () {
+            arrayUsers.splice(
+                arrayUsers.indexOf(socket.Username), 1
+            );
+            socket.broadcast.emit("server-send-listUser", arrayUsers);
+        });
+
+        socket.on("user-send-message", function (data) {
+            io.sockets.emit("server-send-mesage", {
+                un: socket.Username,
+                nd: data
             });
         });
 
-        // when the client emits 'add user', this listens and executes
-        socket.on('add user', (username) => {
-
-            if (addedUser) return;
-
-            // we store the username in the socket session for this client
-            socket.username = username;
-            ++numUsers;
-            addedUser = true;
-            socket.emit('login', {
-                numUsers: numUsers
-            });
-            // echo globally (all clients) that a person has connected
-            socket.broadcast.emit('user joined', {
-                username: socket.username,
-                numUsers: numUsers
-            });
+        socket.on("typing message", function () {
+            var s = socket.Username + " is typing...";
+            socket.broadcast.emit("had typing", s);
         });
 
-        // when the client emits 'typing', we broadcast it to others
-        socket.on('typing', () => {
-            socket.broadcast.emit('typing', {
-                username: socket.username
-            });
+        socket.on("stop typing", function () {
+            io.sockets.emit("who stop typing");
         });
 
-        // when the client emits 'stop typing', we broadcast it to others
-        socket.on('stop typing', () => {
-            socket.broadcast.emit('stop typing', {
-                username: socket.username
-            });
+        socket.on("typing in Room", function () {
+            var s = socket.Username + " is typing...";
+            //socket.broadcast.emit("had typing", s);
+            socket.broadcast.in(socket.Phong).emit("had typing", s);
         });
 
         // when the user disconnects.. perform this
@@ -66,17 +65,49 @@ var chatSocket = function (io) {
             console.log('connect successful');
         });
 
-        socket.on('auto connect', () =>{
+        socket.on('auto connect', (data) => {
             var currentTime = getCurrentTime();
+            io.sockets.emit('test connection', {
+                currentTime: currentTime,
+                posture: data.posture
+            });
+        });
+
+        //Create room
+        socket.on("create room", function (data) {
+            socket.join(data);
+            socket.Phong = data;
+
+            if (!listRooms.includes(data)) {
+                listRooms.push(data);
+            }
+
+            io.sockets.emit("server-send-rooms", listRooms);
+            socket.emit("current room", data);
+
+        });
+
+        //Leave room
+        socket.on("leave room", function (data) {
+            socket.leave(data);
+        });
+
+        //chat in room
+        socket.on("chatRoom", function (data) {
+            console.log(io.sockets.adapter.rooms);
+            io.sockets.in(socket.Phong).emit("serverChatRoom", {
+                un: socket.Username,
+                nd: data
+            });
         });
     });
 }
 
 var getCurrentTime = function () {
-    var today = new Date();    
+    var today = new Date();
     var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     return time;
-  }
+}
 
 module.exports = {
     chatSocket
