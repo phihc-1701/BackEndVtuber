637be831b23e1c76652979e87965ffaac37217d7
phi.hoang <hoang.cao.phi@sun-asterisk.com>
2019-10-10 10:24:04 +0700

    Implement webTRC Chat voice

--- public/main.js
+++ public/main.js
@@ -1,139 +1,242 @@
-var socket = io("http://localhost:3000");
 
-socket.on("REGISTER_USER_EXIST", function () {
-  alert("User has been register !");
-});
-
-socket.on("SERVER_RESPONSE_LISTUSERS", function (data) {
-  $("#boxContent").html("");
-  data.forEach(function (i) {
-    $("#boxContent").append("<div class='user'>" + i + "</div>");
-  });
-});
-
-socket.on("REGISTER_USER_SUCCESS", function (data) {
-  $("#currentUser").html(data);
-  $("#loginForm").hide(2000);
-  $("#chatForm").show(1000);
-});
-
-socket.on("SEND_MESSAGE", function (data) {
-  $("#listMessages").append("<div class='ms'>" + data.username + ":" + data.message + "</div>");
-});
-
-socket.on("TYPING_MESSAGE", function (data) {
-  $("#notification").html("<img width='20px' src='typing05.gif'> " + data);
-});
-
-socket.on("STOP_TYPING_MESSAGE", function () {
-  $("#notification").html("");
-});
-
-socket.on("CURRENT_ROOM", function (data) {
-  $(".currentRoom").html(data);
-});
-
-socket.on("SERVER_SENDMESSAGE_ROOM", function (data) {
-  $("#listMessages").append("<div class='ms'>" + data.username + ":" + data.message + "</div>");
-});
-
-socket.on('SEND_POSTURE', (data) => {
-  $("#postureMessage").append("<div class='ms'>" + "Server receive message " + data.currentTime + "</div>");
-  $("#postureMessage").append("<div class='ms'>" + "Data:  " + data.posture + "</div>");
-  $("#postureMessage").append("<div class='ms'>" + "Client stop emit " + getCurrentTime() + "</div>");
-});
-
-socket.on('disconnect', () => {
-  $("#listMessages").append("<div class='ms'>" + 'You have been disconnected' + "</div>");  
-});
-
-socket.on('reconnect', () => {
-  $("#listMessages").append("<div class='ms'>" + 'You have been reconnected' + "</div>");
-});
-
-socket.on('reconnect_error', () => {
-  $("#listMessages").append("<div class='ms'>" + 'Attempt to reconnect has failed' + "</div>");
-});
-
-const getCurrentTime = () => {
-  var today = new Date();
-  var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-  return ' ' + time;
-}
-
-const sendMessageAll = () => {
-
-  var currentRoom = $(".currentRoom").text();
-  var message = $("#txtMessage").val().trim();
-  if (currentRoom === "All") {
-    socket.emit("SEND_MESSAGE", {message: message});
-  } else {
-    socket.emit("CLIENT_SENDMESSAGE_ROOM", {message: message});
-  }
-
-  $("#txtMessage").val("");
-}
 
 $(document).ready(function () {
-  var interval;
-  $("#loginForm").show();
-  $("#chatForm").hide();
+  var socket = io("http://localhost:3000");
 
-  $("#txtMessage").focusin(function () {
-    var currentRoom = $(".currentRoom").text();
-    if (currentRoom === "All") {
-      socket.emit("TYPING_MESSAGE");
-    } else {
-      socket.emit("TYPING_MESSAGE_IN_ROOM");
+  // CHAT VOICE VARIABLE START
+  var isStreamer;
+  var streamerStream;
+
+  var streamerRtcPeerConnection;
+  var viewerRtcPeerConnection;
+
+  var iceServers = {
+    'iceServers': [
+      { 'url': 'stun:stun.services.mozilla.com' },
+      { 'url': 'stun:stun.l.google.com:19302' }
+    ]
+  }
+
+  var streamConstrains = window.constraints = {
+    audio: true,
+    video: false
+  };
+  // CHAT VOICE VARIABLE END
+
+  // CHAT VOICE FUNCTION START
+  function getStreamerMediaSuccess(stream) {
+    const audioTracks = stream.getAudioTracks();
+    console.log('Got stream with constraints:', constraints);
+    console.log('Using audio device: ' + audioTracks[0].label);
+    stream.oninactive = function () {
+      console.log('Stream ended');
+    };
+    streamerStream = stream;
+    document.getElementById("streamer-audio").srcObject = stream;
+  }
+
+  function handleStreamerMediaError(error) {
+    console.log('navigator.MediaDevices.getUserMedia error: ', error.message, error.name);
+  }
+
+  function viewOnAddStream(event) {
+    remoteStream = event.stream;
+    document.getElementById("viewer-audio").srcObject = event.stream;
+  }
+
+  function onIceCandidate(event) {
+    if (event.candidate) {
+      console.log('send ice candidate');
+      socket.emit('ADD_CANDIDATE', {
+        type: 'candidate',
+        label: event.candidate.sdpMLineIndex,
+        id: event.candidate.sdpMid,
+        candidate: event.candidate.candidate
+      });
+    }
+  }
+
+  function setStreamerLocalAndOffer(sessionDescription) {
+    console.log(sessionDescription);
+    streamerRtcPeerConnection.setLocalDescription(sessionDescription);
+    socket.emit('OFFER', {
+      type: 'offer',
+      sdp: sessionDescription
+    });
+  }
+
+  function setViewerLocalAndAnswer(sessionDescription) {
+    viewerRtcPeerConnection.setLocalDescription(sessionDescription);
+    socket.emit('ANSWER', {
+      type: 'answer',
+      sdp: sessionDescription
+    });
+  }
+  // CHAT VOICE FUNCTION END
+
+  //SOCKET EVENT
+  socket.on("REGISTER_USER_EXIST", function () {
+    alert("User has been register !");
+  });
+
+  socket.on("REGISTER_USER_SUCCESS", function (data) {
+    if (isStreamer) {
+      navigator.mediaDevices.getUserMedia(streamConstrains).then(getStreamerMediaSuccess).catch(handleStreamerMediaError);
+    }
+  });
+
+  socket.on("SEND_MESSAGE", function (data) {
+    if(data.username == $txtUsername.val())
+    {
+      $listMessages.append("<div class='message-area' style= 'text-align: left'> <span class = 'message-text-self'>"  + data.message + "</span></div>");
+    }
+    else{
+      $listMessages.append("<div class='message-area' style= 'text-align: right'> <span class = 'message-text-other'>" + "<span style='color: #f39c12'>"+ data.username +"</span>" + ": " + data.message + "</span> </div>");
+    }
+  });
+
+  socket.on("TYPING_MESSAGE", function (data) {
+    $("#notification").html("<img width='20px' src='typing05.gif'> " + data.typing);
+  });
+
+  socket.on("STOP_TYPING_MESSAGE", function () {
+    $("#notification").html("");
+  });
+
+  socket.on('SEND_POSTURE', (data) => {
+    $("#postureMessage").append("<div class='ms'>" + "Server receive message " + data.currentTime + "</div>");
+    $("#postureMessage").append("<div class='ms'>" + "Client stop emit " + getCurrentTime() + "</div>");
+  });
+
+  socket.on('disconnect', () => {
+    $("#listMessages").append("<div class='ms'>" + 'You have been disconnected' + "</div>");
+  });
+
+  socket.on('reconnect', () => {
+    $("#listMessages").append("<div class='ms'>" + 'You have been reconnected' + "</div>");
+  });
+
+  socket.on('reconnect_error', () => {
+    $("#listMessages").append("<div class='ms'>" + 'Attempt to reconnect has failed' + "</div>");
+  });
+
+  // ----------CHAT VOICE SOCKET EVENT-------------------
+  socket.on("CHAT_VOICE", function () {
+    if (isStreamer) {  
+      streamerRtcPeerConnection = new RTCPeerConnection(iceServers);
+      streamerRtcPeerConnection.onicecandidate = onIceCandidate;
+      streamerRtcPeerConnection.addStream(streamerStream);
+      streamerRtcPeerConnection.createOffer(setStreamerLocalAndOffer, function (e) { console.log(e) })
+    }
+  });
+
+  socket.on('OFFER', (event) => {
+    if (!isStreamer) {
+      viewerRtcPeerConnection = new RTCPeerConnection(iceServers);
+      viewerRtcPeerConnection.onicecandidate = onIceCandidate;
+      viewerRtcPeerConnection.onaddstream = viewOnAddStream;
+      viewerRtcPeerConnection.setRemoteDescription(new RTCSessionDescription(event));
+      viewerRtcPeerConnection.createAnswer(setViewerLocalAndAnswer, function (e) { console.log(e) })
+    }
+  });
+
+  socket.on('ANSWER', (event) => {
+    if (isStreamer) {
+      streamerRtcPeerConnection.setRemoteDescription(new RTCSessionDescription(event));
+    }
+  });
+
+  socket.on('ADD_CANDIDATE', (event) => {
+    var candidate = new RTCIceCandidate({
+      sdpMLineIndex: event.label,
+      candidate: event.candidate
+    });
+    if (isStreamer) {
+      streamerRtcPeerConnection.addIceCandidate(candidate);
+    }
+    else {
+      viewerRtcPeerConnection.addIceCandidate(candidate);
+    }
+  });
+
+  // EVENT ON PAGE
+  var $audioStreamer = $("#streamer-audio");
+  var $audioViewer = $("#viewer-audio");
+  
+
+  var $btnStreamer = $("#btnStreamer");
+  var $btnViewer = $("#btnViewer");
+
+  var $listMessages = $("#listMessages");
+  var $postureMessage = $("#postureMessage")
+
+  var $btnSendMessage = $("#btnSendMessage");
+
+  var $btnAutoSendPosture = $("#btnAutoSendPosture");
+  var $btnStopPosture = $("#btnStopPosture");
+
+  var $txtUsername = $("#txtUsername");
+  var $txtMessage = $("#txtMessage");
+
+  var $divChatRoom = $("#chatRoom");
+  var $divRegisterUser = $("#registerUser");
+  var $lblCurrentUser = $("#currentUser");
+
+
+  $btnStreamer.click(function () {
+    if (validateUserName()) {
+      $divChatRoom.show();
+      $divRegisterUser.hide();
+      $lblCurrentUser.text("streamer: " + $txtUsername.val());
+      $audioViewer.hide();
+      socket.emit("REGISTER_USER", { username: $txtUsername.val(), role: "Streamer" });
+      isStreamer = true;
+    }
+  });
+
+  $btnViewer.click(function () {
+    if (validateUserName()) {
+      $divChatRoom.show();
+      $divRegisterUser.hide();
+      $btnAutoSendPosture.hide();
+      $btnStopPosture.hide();
+      $lblCurrentUser.text("viewer: " + $txtUsername.val());
+      $audioStreamer.hide();
+      socket.emit("REGISTER_USER", { username: $txtUsername.val(), role: "Viewer" });
+      isStreamer = false;
+      socket.emit('CHAT_VOICE');
+    }
+  });
+
+  function validateUserName() {
+    if ($txtUsername.val() === '') {
+      alert('Please input username !!');
+
+      return false;
     }
 
+    return true;
+  }
+
+  $txtMessage.focusin(function () {
+    socket.emit("TYPING_MESSAGE");
   })
 
-  $("#txtMessage").focusout(function () {
+  $txtMessage.focusout(function () {
     socket.emit("STOP_TYPING_MESSAGE");
   })
 
-  $("#btnRegister").click(function () {
-    var role = $('#roles option:selected').text();
-    if (role === "User")
-      $("#btnCreateRoom").val("Join Room");
-    socket.emit("REGISTER_USER", {username: $("#txtUsername").val()});
-  });
-
-  $("#btnLogout").click(function () {
-    socket.emit("LOGOUT");
-    $("#chatForm").hide(2000);
-    $("#loginForm").show(1000);
-  });
-
-  $("#txtMessage").keydown(function () {
+  $txtMessage.keydown(function () {
     if (event.which === 13)
       sendMessageAll();
   });
 
-  $("#btnSendMessage").click(function () {
+  $btnSendMessage.click(function () {
     sendMessageAll();
   });
 
-  $("#btnCreateRoom").click(function () {
-    socket.emit("JOIN_ROOM", $("#txtRoom").val());
-  });
-
-  $("#btnLeaveRoom").click(function () {
-    socket.emit("LEAVE_ROOM", $("#txtRoom").val());
-    $(".currentRoom").html("All");
-  });
-
-  $("#btnAutoSendPosture").click(function () {
-
-    var chatInfo = {
-      "roomID": 123,
-      "userID": 1,
-      "userName": "MrA",
-      "messenger": "Hello"
-    };
-
+  var interval;
+  $btnAutoSendPosture.click(function () {
     var posture = {
       "roomID": 123,
       "userType": 1,
@@ -153,14 +256,27 @@ $(document).ready(function () {
     };
 
     interval = setInterval(function () {
-      $("#postureMessage").append("<div class='ms'>" + "Client start emit " + getCurrentTime() + "</div>");
+      $postureMessage.append("<div class='ms'>" + "Client start emit " + getCurrentTime() + "</div>");
       console.log("send posture" + getCurrentTime());
       socket.emit('SEND_POSTURE', posture);
     }, 100);
 
   });
 
-  $("#btnStopPosture").click(function () {
+  $btnStopPosture.click(function () {
     clearInterval(interval);
   });
+
+  const sendMessageAll = () => {
+    var message = $txtMessage.val().trim();
+    socket.emit("SEND_MESSAGE", { message: message });
+    $txtMessage.val("");
+  }
+
+  const getCurrentTime = () => {
+    var today = new Date();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    return ' ' + time;
+  }
+
 });
--- socket.io/chat.socket.js
+++ socket.io/chat.socket.js
@@ -2,30 +2,25 @@ var EVENTS = require('../common/constant').CONSTANTS.SOCKET_EVENTS;
 
 var chatSocket = function (io) {
 
-    //Variable declare area
-    var listUserActived = [];
-
     io.on(EVENTS.BASE.CONNECTION, (socket) => {
-        var listRooms = [];
-
         //---Event registration area---
         //Connection event handle
         socket.on(EVENTS.INIT.REGISTER_USER, function (userInfo) {
-            listUserActived.push(userInfo.username);
-                socket.UserName = userInfo.username;
-                socket.emit(EVENTS.INIT.REGISTER_USER_SUCCESS, { username: userInfo.username });
+            socket.UserName = userInfo.username;
+            socket.Role = userInfo.role;
+            socket.emit(EVENTS.INIT.REGISTER_USER_SUCCESS, { username: userInfo.username });
         });
 
         //Event user is typing
         socket.on(EVENTS.CHATTING.TYPING_MESSAGE, function () {
             var whoTyping = socket.UserName + EVENTS.IS_TYPING;
-            io.sockets.emit(EVENTS.CHATTING.TYPING_MESSAGE, {typing: whoTyping});
+            socket.broadcast.emit(EVENTS.CHATTING.TYPING_MESSAGE, { typing: whoTyping });
         });
 
         //Event user is stopping type
         socket.on(EVENTS.CHATTING.STOP_TYPING_MESSAGE, function () {
             var whoStopTyping = socket.UserName + " stop" + EVENTS.IS_TYPING;
-            io.sockets.emit(EVENTS.CHATTING.STOP_TYPING_MESSAGE, {stopTyping: whoStopTyping});
+            socket.broadcast.emit(EVENTS.CHATTING.STOP_TYPING_MESSAGE, { stopTyping: whoStopTyping });
         });
 
         // Event send message all
@@ -45,58 +40,32 @@ var chatSocket = function (io) {
             });
         });
 
-        // -------------------------------------------
-        //LIST EVENT IN ROOM
-        //Event send message in Room
-        socket.on(EVENTS.CLIENT_SENDMESSAGE_ROOM, function (message) {
-            console.log(io.sockets.adapter.rooms);
-            io.sockets.in(socket.Phong).emit(EVENTS.SERVER_SENDMESSAGE_ROOM, {
-                username: socket.UserName,
-                message: message.message
-            });
+        //EVENT FOR WEB RTC P2P   
+        socket.on(EVENTS.WEB_RTC.CHAT_VOICE, () => {
+            socket.broadcast.emit(EVENTS.WEB_RTC.CHAT_VOICE);
         });
 
-        //Event user is typing in Room
-        socket.on(EVENTS.TYPING_MESSAGE_IN_ROOM, function () {
-            var istyping = socket.UserName + EVENTS.IS_TYPING;
-            socket.broadcast.in(socket.Phong).emit(EVENTS.TYPING_MESSAGE, istyping);
+        socket.on(EVENTS.WEB_RTC.ADD_CANDIDATE, (event) => {
+            socket.broadcast.emit(EVENTS.WEB_RTC.ADD_CANDIDATE, event);
         });
 
-        //EVENT FROM JOIN/LEAVE ROOM
-        //Event join room
-        socket.on(EVENTS.JOIN_ROOM, function (data) {
-            socket.join(data);
-            socket.Phong = data;
-
-            if (!listRooms.includes(data)) {
-                listRooms.push(data);
-            }
-            socket.emit(EVENTS.CURRENT_ROOM, data);
+        socket.on(EVENTS.WEB_RTC.OFFER, (event) => {
+            socket.broadcast.emit(EVENTS.WEB_RTC.OFFER, event.sdp);
         });
 
-        //Event Leave room
-        socket.on(EVENTS.LEAVE_ROOM, function (data) {
-            socket.leave(data);
+        socket.on(EVENTS.WEB_RTC.ANSWER, (event) => {
+            socket.broadcast.emit(EVENTS.WEB_RTC.ANSWER, event.sdp);
         });
-
-        //Event log out
-        socket.on(EVENTS.LOGOUT, function () {
-            listUserActived.splice(
-                listUserActived.indexOf(socket.UserName), 1
-            );
-            socket.broadcast.emit(EVENTS.SERVER_RESPONSE_LISTUSERS, listUserActived);
-        });
+        // CHAT VOICE   
 
         //Event handle disconnect when user disconnected
         socket.on(EVENTS.BASE.DISCONNECTED, () => {
-            listUserActived.splice(
-                listUserActived.indexOf(socket.UserName), 1
-            );
-            socket.broadcast.emit(EVENTS.SERVER_RESPONSE_LISTUSERS, listUserActived);
+
         });
     });
 }
 
+//UTILITIES FUNCTION
 var getCurrentTime = function () {
     var today = new Date();
     var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
